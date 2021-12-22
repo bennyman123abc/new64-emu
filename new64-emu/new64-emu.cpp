82db231aee17a725dc27cf63d53ed53ceca3e76b
Ben <bennyman123abc@hotmail.com>
2021-12-22 01:00:25 -0600

    Add project files.

---
+++ new64-emu/new64-emu.cpp
@@ -0,0 +1,12 @@
+﻿// new64-emu.cpp : Defines the entry point for the application.
+//
+
+#include "new64-emu.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
